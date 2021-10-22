c63965ffbcb97ed9e548c206483844a2d1d8f628
Varvara Yakovleva <yakovleva.varvaraa@gmail.com>
2021-10-22 22:07:49 +0300

    Доделана 1 часть лабы 5

---
+++ Laba 5/Part_1/user.rb
@@ -0,0 +1,12 @@
+require './main'
+
+puts 'Введите значение x'
+x = gets.chomp
+puts 'Введите значение z'
+z = gets.chomp
+
+if calculate(x, z)
+  puts "y = #{calculate(x, z)}"
+else
+  puts "y не определен!"
+end

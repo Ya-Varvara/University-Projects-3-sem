9f99f32f29afc2fa9d83a0b1b87f3c5d77376c96
Varvara Yakovleva <yakovleva.varvaraa@gmail.com>
2021-10-24 15:22:28 +0300

    Доработаны тесты

--- Laba 5/Part_2/test.rb
+++ Laba 5/Part_2/test.rb
@@ -1,27 +1,30 @@
 require 'minitest/autorun'
 require './main'
 
-def Generate_Str(orig_str, ch_str, arr_w)
-  ch_str = (0..2 + rand(5)).map { (0..5 + rand(10)).map { (65 + rand(26)).chr }.join }
-  ch_str.each_with_index do |word, index|
-    if index == rand(ch_str.size)
+def Generate_random_string(len)
+  (0..3 + rand(len)).map {(0..5 + rand(10)).map { (65 + rand(26)).chr }.join}.join(" ");
+end
+
+def Generate_array_and_str(orig_s, change_s, arr)
+  change_s.split.each_with_index do |word, index|
+    if rand(change_s.split.length) == index
       new_word = "#" + (0..3 + rand(5)).map{(65 + rand(26)).chr}.join + "#"
-      arr_w << word
-      orig_str << new_word
+      arr << word
+      orig_s << new_word
     else
-      orig_str << word
+      orig_s << word
     end
   end
-  ch_str.join(' ')
-  orig_str.join(' ')
 end
 
 class TestChangeString < Minitest::Unit::TestCase
   def test_1
-    orig_str = ""
-    change_str = ""
+    orig_str = []
+    change_str = Generate_random_string(5)
     arr_w = []
-    Generate_Str(orig_str, change_str, arr_w)
-    assert_equal(change_str, ChangeString(orig_str, arr_w))
+    Generate_array_and_str(orig_str, change_str, arr_w)
+    assert_equal(change_str, ChangeString(orig_str.join(' '), arr_w))
   end
 end
+
+

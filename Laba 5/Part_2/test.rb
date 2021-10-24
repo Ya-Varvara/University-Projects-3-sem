b560ce152de36f9d551bd17d6dcaf166ec8377e8
iSerEzhik <74928925+iSerEzhik@users.noreply.github.com>
2021-10-24 14:36:12 +0300

    Моё предложение в part5/test.rb

--- Laba 5/Part_2/test.rb
+++ Laba 5/Part_2/test.rb
@@ -1,19 +1,19 @@
 require 'minitest/autorun'
 require './main'
 
-def Generate_Str(orig_str, ch_str, arr_w)
+def Generate_Str(orig_str, ch_str1, arr_w)
   ch_str = (0..2 + rand(5)).map { (0..5 + rand(10)).map { (65 + rand(26)).chr }.join }
   ch_str.each_with_index do |word, index|
     if index == rand(ch_str.size)
       new_word = "#" + (0..3 + rand(5)).map{(65 + rand(26)).chr}.join + "#"
       arr_w << word
-      orig_str << new_word
+      orig_str << new_word << ' '
     else
-      orig_str << word
+      orig_str << word << ' '
     end
   end
-  ch_str.join(' ')
-  orig_str.join(' ')
+  orig_str.strip!
+  ch_str1<<ch_str.join(' ')
 end
 
 class TestChangeString < Minitest::Unit::TestCase
@@ -22,6 +22,8 @@ class TestChangeString < Minitest::Unit::TestCase
     change_str = ""
     arr_w = []
     Generate_Str(orig_str, change_str, arr_w)
+    p change_str
+    p orig_str
     assert_equal(change_str, ChangeString(orig_str, arr_w))
   end
 end

ffda9de85edd27ede72272a85ae2381c61b46c64
iSerEzhik <74928925+iSerEzhik@users.noreply.github.com>
2021-10-24 15:45:54 +0300

    Моё предложение в part5/test.rb

--- Laba 5/Part_2/test.rb
+++ Laba 5/Part_2/test.rb
@@ -1,29 +1,47 @@
 require 'minitest/autorun'
 require './main'
 
-def Generate_random_string(len)
-  (0..3 + rand(len)).map {(0..5 + rand(10)).map { (65 + rand(26)).chr }.join}.join(" ");
+MIN_WORD_LEN = 5
+MAX_WORD_LEN = MIN_WORD_LEN + 10
+MIN_STRING_LEN = 3
+MAX_STRING_LEN = MIN_STRING_LEN + 5
+MIN_CHAR = 65
+MAX_CHAR = MIN_CHAR + 26
+
+def generate_random_char
+  (MIN_CHAR + rand(MAX_CHAR - MIN_CHAR)).chr
 end
 
-def Generate_array_and_str(orig_s, change_s, arr)
-  change_s.split.each_with_index do |word, index|
-    if rand(change_s.split.length) == index
-      new_word = "#" + (0..3 + rand(5)).map{(65 + rand(26)).chr}.join + "#"
-      arr << word
-      orig_s << new_word
+def generate_random_word
+  (0..MIN_WORD_LEN + rand(MAX_WORD_LEN - MIN_WORD_LEN)).map { generate_random_char }.join
+end
+
+def generate_random_string
+  (0..MIN_STRING_LEN + rand(MAX_STRING_LEN - MIN_STRING_LEN)).map { generate_random_word }.join(" ");
+end
+
+def generate_array_and_str
+  words_arr = []
+  string_for_change = ""
+  expected_string = ""
+  random_string = generate_random_string
+  random_string.split.each_with_index do |word, index|
+    if rand(random_string.split.length) == index
+      new_word = "##{generate_random_word}#"
+      words_arr << word
+      string_for_change << new_word << ' '
     else
-      orig_s << word
+      string_for_change << word << ' '
     end
+    expected_string << word << ' '
   end
+  [string_for_change.strip, words_arr, expected_string.strip]
 end
 
 class TestChangeString < Minitest::Unit::TestCase
   def test_1
-    orig_str = []
-    change_str = Generate_random_string(5)
-    arr_w = []
-    Generate_array_and_str(orig_str, change_str, arr_w)
-    assert_equal(change_str, ChangeString(orig_str.join(' '), arr_w))
+    string_to_change, words_arr, expected_string = generate_array_and_str
+    assert_equal(expected_string, ChangeString(string_to_change, words_arr))
   end
 end
 

87335126b350410e661da36189224e7975c72e9f
iSerEzhik <74928925+iSerEzhik@users.noreply.github.com>
2021-10-24 15:45:54 +0300

    Моё предложение в part5/test.rb

--- Laba 5/Part_2/test.rb
+++ Laba 5/Part_2/test.rb
@@ -1,29 +1,39 @@
 require 'minitest/autorun'
 require './main'
 
-def Generate_random_string(len)
-  (0..3 + rand(len)).map {(0..5 + rand(10)).map { (65 + rand(26)).chr }.join}.join(" ");
+GENERATED_WORD_LEN = 0..5 + rand(10)
+GENERATED_STRING_LEN = 0..3 + rand(5)
+
+def generate_random_word
+  (GENERATED_WORD_LEN).map { (65 + rand(26)).chr }.join
 end
 
-def Generate_array_and_str(orig_s, change_s, arr)
-  change_s.split.each_with_index do |word, index|
-    if rand(change_s.split.length) == index
-      new_word = "#" + (0..3 + rand(5)).map{(65 + rand(26)).chr}.join + "#"
-      arr << word
-      orig_s << new_word
+def generate_random_string
+  (GENERATED_STRING_LEN).map { generate_random_word }.join(" ");
+end
+
+def generate_array_and_str
+  words_arr = []
+  string_for_change = ""
+  expected_string = ""
+  random_string = generate_random_string
+  random_string.split.each_with_index do |word, index|
+    if rand(random_string.split.length) == index
+      new_word = "##{generate_random_word}#"
+      words_arr << word
+      string_for_change << new_word << ' '
     else
-      orig_s << word
+      string_for_change << word << ' '
     end
+    expected_string << word << ' '
   end
+  [string_for_change.strip, words_arr, expected_string.strip]
 end
 
 class TestChangeString < Minitest::Unit::TestCase
   def test_1
-    orig_str = []
-    change_str = Generate_random_string(5)
-    arr_w = []
-    Generate_array_and_str(orig_str, change_str, arr_w)
-    assert_equal(change_str, ChangeString(orig_str.join(' '), arr_w))
+    string_to_change, words_arr, expected_string = generate_array_and_str
+    assert_equal(expected_string, ChangeString(string_to_change, words_arr))
   end
 end
 

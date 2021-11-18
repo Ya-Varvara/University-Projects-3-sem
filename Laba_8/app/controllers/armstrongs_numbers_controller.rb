8de0fa781e4bcb647a4a8860dd8cbd4a711dd3e2
Varvara Yakovleva <yakovleva.varvaraa@gmail.com>
2021-11-18 19:48:38 +0300

    commit laba_8

---
+++ Laba_8/app/controllers/armstrongs_numbers_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class ArmstrongsNumbersController < ApplicationController
+  def input; end
+
+  def view
+    @received = params[:v1].to_i
+    if @received >= 1 && @received <= 9
+      @numbers = find_numbers(@received.to_i)
+      @result = "Числа Армстронга длиной #{@received}"
+    else
+      @result = 'Введенная длина числа некорректна!'
+      @numbers = []
+    end
+  end
+end
+
+def num_degree(len)
+  (0..9).map { |x| x**len }
+end
+
+def armstr(x, deg)
+  x.digits(10).inject(0) { |sum, n| sum + deg[n.to_i] } == x
+end
+
+def find_numbers(length)
+  deg = num_degree(length)
+  numbers = []
+  (10**(length - 1)..(10**length)).each do |x|
+    numbers.push([x, x.to_s.split('').map { |n| "#{n}^#{length}" }.join("\s+\s")]) if armstr(x, deg)
+  end
+  numbers
+end
---
+++ Laba_8/test/controllers/armstrongs_numbers_controller_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+
+class ArmstrongsNumbersControllerTest < ActionController::TestCase
+  test 'should get input' do
+    get :input
+    assert_response :success
+  end
+
+  test 'should get view' do
+    get :view
+    assert_response :success
+  end
+
+  test 'should get 4 numbers for view with 3' do
+    expected = [[153, '1^3 + 5^3 + 3^3'], [370, '3^3 + 7^3 + 0^3'], [371, '3^3 + 7^3 + 1^3'], [407, '4^3 + 0^3 + 7^3']]
+    get :view, params: { v1: 3 }
+    assert_equal expected, assigns[:numbers]
+  end
+
+  test 'should get from view with empty' do
+    expected = 'Введенная длина числа некорректна!'
+    get :view, params: { v1: '' }
+    assert_equal expected, assigns[:result]
+  end
+end

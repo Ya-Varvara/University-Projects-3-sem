e2f4ae6cc2ccbde99e656fcfe3ee467ab7d564ca
Varvara Yakovleva <yakovleva.varvaraa@gmail.com>
2021-12-17 01:34:06 +0300

    Laba 10 with test

--- Laba_8/second/test/controllers/armst_controller_test.rb
+++ Laba_8/second/test/controllers/armst_controller_test.rb
@@ -1,13 +1,29 @@
-require "test_helper"
+require_relative '../test_helper'
 
-class ArmstControllerTest < ActionDispatch::IntegrationTest
-  test "should get input" do
-    get armst_input_url
+# class ArmstControllerTest < ActionDispatch::IntegrationTest
+class ArmstControllerTest < ActionController::TestCase
+  test 'should get input' do
+    get :input
     assert_response :success
   end
 
-  test "should get view" do
-    get armst_view_url
+  test 'should get view' do
+    get :view
     assert_response :success
+  end
+
+  test 'should get HTML content type' do
+    get :view, params: { v1: '', side: 'server' }
+    assert_equal('text/html', @response.content_type.split(';')[0], 'Received another format')
+  end
+
+  test 'should get XML content type' do
+    get :view, params: { v1: '', side: 'client' }
+    assert_equal('application/xml', @response.content_type.split(';')[0], 'Received another format')
+  end
+
+  test 'should get XML type with XSLT' do
+    get :view, params: { v1: '', side: 'client-with-xslt' }
+    assert_equal('application/xml', @response.content_type.split(';')[0], 'Received another format')
   end
 end
